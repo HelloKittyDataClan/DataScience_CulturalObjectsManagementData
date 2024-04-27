b530cba59f30c837e094214ad582ba90d0f6209c
Beatrice Bucci <99805412+BeatriceBucci@users.noreply.github.com>
2024-04-27 17:43:36 +0200

    Update dataModel.py

--- dataModel.py
+++ dataModel.py
@@ -9,13 +9,20 @@ class IdentifiableEntity(object): #identifichiamo l'ID
     
 
 class CulturalObject(IdentifiableEntity):
-    def __init__(self, id:str, title:str, date:str= None, owner:str, place:str):  #vado a definire title, date, owner e place del mio csv
+    def __init__(self, id:str, title:str, date:str= None, owner:str, place:str): #vado a definire title, date, owner e place del mio csv
+        super().__init__(id)  #cosi facendo vado a richiamare l'ID della classe IdentifiableEntity
+        if not isinstance(title, str):
+            raise ValueError("Title must be a string for the CulturalObject")
+        if not isinstance(owner, str):
+            raise ValueError("Owner must be a string for the CulturalObject")
+        if not isinstance(place, str):
+            raise ValueError("Place must be a string for the CulturalObject")
         self.title=title
         self.date=date
         self.owner=owner
         self.place=place
         self.authors=[]
-        super().__init__(id) #cosi facendo vado a richiamare l'ID della classe IdentifiableEntity
+        
 
     def getTitle(self):
         return self.title
@@ -32,8 +39,12 @@ class CulturalObject(IdentifiableEntity):
     def getAuthors(self):
         return self.authors
 
-    def addAuthor(self, author: Person):
-        self.authors.append(author)
+    def addAuthor(self, author):
+        if isinstance(author, Person):
+            self.authors.append(author)
+        else:
+            raise ValueError("Author must be an instance of Person")
+
         
 #definiamo le sottoclassi relative alla classe Cultrual Object   
 class NauticalChart(CulturalObject):
