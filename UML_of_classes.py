a478a71701699421efac72646c47a45504170246
Chiaramartina <106493111+Chiaramartina@users.noreply.github.com>
2024-06-14 20:21:27 +0200

    Update UML_of_classes.py

--- UML_of_classes.py
+++ UML_of_classes.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import csv
 import urllib.request
+from sparql_dataframe import get
 
 
 
@@ -18,11 +19,8 @@ class Handler:  # Chiara
         return self.dbPathOrUrl
 
     def setDbPathOrUrl(self, pathOrUrl: str) -> bool:
-        if isinstance(pathOrUrl, str):
-            self.dbPathOrUrl = pathOrUrl
-            return True
-        else:
-            return False
+        self.dbPathOrUrl = pathOrUrl
+        return self.dbPathOrUrl == pathOrUrl
 
 class UploadHandler(Handler):
     def __init__(self):
@@ -33,152 +31,173 @@ class UploadHandler(Handler):
 
 class MetadataUploadHandler(UploadHandler):  # Chiara
     def __init__(self):
-        super().__init__()
+        self.dbPathOrUrl = ""
 
-    def pushDataToDb(self, path):
-        my_graph = Graph()
-        # Read the data from the csv file and store them into a dataframe
-        venus = pd.read_csv(path,
-                            keep_default_na=False,
-                            dtype={
-                                "Id": "string",
-                                "Type": "string",
-                                "Title": "string",
-                                "Date": "string",
-                                "Author": "string",
-                                "Owner": "string",
-                                "Place": "string",
-                            })
-    
-        # Define namespaces
-        base_url = Namespace("http://github.com/HelloKittyDataClan/DSexam/")
-        db = Namespace("https://dbpedia.org/property/")
-        schema = Namespace("http://schema.org/")
+    def pushDataToDb(self, path) -> bool:
 
-        # Create Graph
-        my_graph.bind("base_url", base_url)
-        my_graph.bind("db", db)
-        my_graph.bind("FOAF", FOAF)
-        my_graph.bind("schema", schema)
-        
-        # Define classes about Cultural Object
-        Person = URIRef(FOAF + "Person")
-        NauticalChart = URIRef(base_url + "NauticalChart")
-        ManuscriptPlate = URIRef(base_url + "ManuscriptPlate")
-        ManuscriptVolume = URIRef(base_url + "ManuscriptVolume")
-        PrintedVolume = URIRef(base_url + "PrintedVolume")
-        PrintedMaterial = URIRef(base_url + "PrintedMaterial")
-        Herbarium = URIRef(db + "Herbarium")
-        Specimen = URIRef(base_url + "Specimen")
-        Painting = URIRef(db + "Painting")
-        Model = URIRef(db + "Model")
-        Map = URIRef(db + "Map")
+            my_graph = Graph()
+            # Read the data from the csv file and store them into a dataframe
+            venus = pd.read_csv(path,
+                                keep_default_na=False,
+                                dtype={
+                                    "Id": "string",
+                                    "Type": "string",
+                                    "Title": "string",
+                                    "Date": "string",
+                                    "Author": "string",
+                                    "Owner": "string",
+                                    "Place": "string",
+                                })
+            # Rimuoviamo i duplicati della colonna id, mantenendo la prima istanza
+            venus.drop_duplicates(subset=["Id"], keep="first", inplace=True, ignore_index=True)
 
-        # Attributes related to classes
-        title = URIRef(schema + "title")
-        date = URIRef(schema + "dateCreated")
-        place = URIRef(schema + "itemLocation")
-        id = URIRef(schema + "identifier")
-        owner = URIRef(base_url + "owner")
+            # Define namespaces
+            base_url = Namespace("http://github.com/HelloKittyDataClan/DSexam/")
+            db = Namespace("https://dbpedia.org/property/")
+            schema = Namespace("http://schema.org/")
 
-        # Relation with authors among classes
-        relAuthor = URIRef(schema + "author")
+            # Create Graph
+            my_graph.bind("base_url", base_url)
+            my_graph.bind("db", db)
+            my_graph.bind("FOAF", FOAF)
+            my_graph.bind("schema", schema)
 
-        # Attributes related to the class Person
-        name = URIRef(FOAF + "name") #uri di FOAF http://xmlns.com/foaf/0.1/
+            # Define classes about Cultural Object
+            Person = URIRef(FOAF + "Person")
+            NauticalChart = URIRef(base_url + "NauticalChart")
+            ManuscriptPlate = URIRef(base_url + "ManuscriptPlate")
+            ManuscriptVolume = URIRef(base_url + "ManuscriptVolume")
+            PrintedVolume = URIRef(base_url + "PrintedVolume")
+            PrintedMaterial = URIRef(base_url + "PrintedMaterial")
+            Herbarium = URIRef(db + "Herbarium")
+            Specimen = URIRef(base_url + "Specimen")
+            Painting = URIRef(db + "Painting")
+            Model = URIRef(db + "Model")
+            Map = URIRef(db + "Map")
 
-        
-        # Add to the graph the Cultural Object
-        for idx, row in venus.iterrows():
-            loc_id = "culturalobject-" + str(idx)
-            subj = URIRef(base_url + loc_id)
-            if row["Id"] != "":
-                my_graph.add((subj, id, Literal(str(row["Id"]))))
-            
-            # Assign a resource class to the object
-            if row["Type"] != "":
-                if row["Type"].lower() == "nautical chart":
-                    my_graph.add((subj, RDF.type, NauticalChart))
-                elif row["Type"].lower() == "manuscript plate":
-                    my_graph.add((subj, RDF.type, ManuscriptPlate))
-                elif row["Type"].lower() == "manuscript volume":
-                    my_graph.add((subj, RDF.type, ManuscriptVolume))
-                elif row["Type"].lower() == "printed volume":
-                    my_graph.add((subj, RDF.type, PrintedVolume))
-                elif row["Type"].lower() == "printed material":
-                    my_graph.add((subj, RDF.type, PrintedMaterial))
-                elif row["Type"].lower() == "herbarium":
-                    my_graph.add((subj, RDF.type, Herbarium))
-                elif row["Type"].lower() == "specimen":
-                    my_graph.add((subj, RDF.type, Specimen))
-                elif row["Type"].lower() == "painting":
-                    my_graph.add((subj, RDF.type, Painting))
-                elif row["Type"].lower() == "model":
-                    my_graph.add((subj, RDF.type, Model))
-                elif row["Type"].lower() == "map":
-                    my_graph.add((subj, RDF.type, Map))
-                
-     
-            # Assign title
-            if row["Title"] != "":
-                my_graph.add((subj, title, Literal(str(row["Title"]))))
-            # Assign date
-            if row["Date"] != "":
-                my_graph.add((subj, date, Literal(str(row["Date"]))))
-            # Assign owner
-            if row["Owner"] != "":
-                my_graph.add((subj, owner, Literal(str(row["Owner"]))))
-            # Assign place
-            if row["Place"] != "":
-                my_graph.add((subj, place, Literal(str(row["Place"]))))
-        
-        # Populating the graph with all the people
-        author_id_mapping = dict()   
-        object_mapping = dict()
-        people_counter = 0
+            # Attributes related to classes
+            title = URIRef(schema + "title")
+            date = URIRef(schema + "dateCreated")
+            place = URIRef(schema + "itemLocation")
+            id = URIRef(schema + "identifier")
+            owner = URIRef(base_url + "owner")
 
-        for idx, row in venus.iterrows():
-            if row["Author"] != "":
-                author_list = row["Author"].split(";")
-                for author in author_list:
-                    if "(" in author and ")" in author:
-                        split_index = author.index("(")
-                        author_name = author[:split_index - 1].strip()
-                        author_id = author[split_index + 1:-1].strip()
-                
-                        object_id = row["Id"]
+            # Relation with authors among classes
+            relAuthor = URIRef(schema + "author")
 
-                        if author_id in author_id_mapping:
-                            person_uri = author_id_mapping[author_id]
-                        else:
-                            local_id = "person-" + str(people_counter)
-                            person_uri = URIRef(base_url + local_id)
-                            my_graph.add((person_uri, RDF.type, Person))
-                            my_graph.add((person_uri, id, Literal(author_id)))
-                            my_graph.add((person_uri, name, Literal(author_name)))
-                            author_id_mapping[author_id] = person_uri
-                            people_counter += 1
+            # Attributes related to the class Person
+            name = URIRef(FOAF + "name")  # URI di FOAF http://xmlns.com/foaf/0.1/
 
-                        if object_id in object_mapping:
-                            object_mapping[object_id].add(person_uri)
-                        else:
-                            object_mapping[object_id] = {person_uri}
+            # Add to the graph the Cultural Object
+            for idx, row in venus.iterrows():
+                loc_id = "culturalobject-" + str(idx)
+                subj = URIRef(base_url + loc_id)
+                if row["Id"] != "":
+                    my_graph.add((subj, id, Literal(str(row["Id"]))))
 
-            # Aggiungi l'assegnazione degli autori al grafo
-            for object_id, authors in object_mapping.items():
-                for author_uri in authors:
-                    my_graph.add((URIRef(base_url + object_id), relAuthor, author_uri))  #modifica leggi alla fine del codice 
+                # Assign a resource class to the object
+                if row["Type"] != "":
+                    if row["Type"].lower() == "nautical chart":
+                        my_graph.add((subj, RDF.type, NauticalChart))
+                    elif row["Type"].lower() == "manuscript plate":
+                        my_graph.add((subj, RDF.type, ManuscriptPlate))
+                    elif row["Type"].lower() == "manuscript volume":
+                        my_graph.add((subj, RDF.type, ManuscriptVolume))
+                    elif row["Type"].lower() == "printed volume":
+                        my_graph.add((subj, RDF.type, PrintedVolume))
+                    elif row["Type"].lower() == "printed material":
+                        my_graph.add((subj, RDF.type, PrintedMaterial))
+                    elif row["Type"].lower() == "herbarium":
+                        my_graph.add((subj, RDF.type, Herbarium))
+                    elif row["Type"].lower() == "specimen":
+                        my_graph.add((subj, RDF.type, Specimen))
+                    elif row["Type"].lower() == "painting":
+                        my_graph.add((subj, RDF.type, Painting))
+                    elif row["Type"].lower() == "model":
+                        my_graph.add((subj, RDF.type, Model))
+                    elif row["Type"].lower() == "map":
+                        my_graph.add((subj, RDF.type, Map))
 
-        # Store RDF data in SPARQL endpoint
-        store = SPARQLUpdateStore()
-        endpoint = self.getDbPathOrUrl() + "sparql"
+                # Assign title
+                if row["Title"] != "":
+                    my_graph.add((subj, title, Literal(str(row["Title"]))))
+                # Assign date
+                if row["Date"] != "":
+                    my_graph.add((subj, date, Literal(str(row["Date"]))))
+                # Assign owner
+                if row["Owner"] != "":
+                    my_graph.add((subj, owner, Literal(str(row["Owner"]))))
+                # Assign place
+                if row["Place"] != "":
+                    my_graph.add((subj, place, Literal(str(row["Place"]))))
 
-        store.open((endpoint, endpoint))  # First parameter reading data, second write database
+            # Populating the graph with all the people
+            author_id_mapping = dict()
+            object_mapping = dict()
+            people_counter = 0
 
-        for triple in my_graph.triples((None, None, None)):
-            store.add(triple)
+            for idx, row in venus.iterrows():
+                if row["Author"] != "":
+                    author_list = row["Author"].split(";")
+                    for author in author_list:
+                        if "(" in author and ")" in author:
+                            split_index = author.index("(")
+                            author_name = author[:split_index - 1].strip()
+                            author_id = author[split_index + 1:-1].strip()
 
-        store.close()
+                            object_id = row["Id"]
+
+                            if author_id in author_id_mapping:
+                                person_uri = author_id_mapping[author_id]
+                            else:
+                                local_id = "person-" + str(people_counter)
+                                person_uri = URIRef(base_url + local_id)
+                                my_graph.add((person_uri, RDF.type, Person))
+                                my_graph.add((person_uri, id, Literal(author_id)))
+                                my_graph.add((person_uri, name, Literal(author_name)))
+                                author_id_mapping[author_id] = person_uri
+                                people_counter += 1
+
+                            if object_id in object_mapping:
+                                object_mapping[object_id].add(person_uri)
+                            else:
+                                object_mapping[object_id] = {person_uri}
+
+                # Aggiungi l'assegnazione degli autori al grafo
+                for object_id, authors in object_mapping.items():
+                    for author_uri in authors:
+                        my_graph.add((URIRef(base_url + object_id), relAuthor, author_uri))  # modifica leggi alla fine del codice
+
+            # Store RDF data in SPARQL endpoint
+            store = SPARQLUpdateStore()
+            endpoint = self.getDbPathOrUrl() + "sparql"
+            store.open((endpoint, endpoint))  # First parameter reading data, second write database
+
+            for triple in my_graph.triples((None, None, None)):
+                store.add(triple)
+
+            store.close()
+
+            # Verificare se il grafo è stato caricato 
+            query_graph = """
+            SELECT ?s ?p ?o
+            WHERE {
+                ?s ?p ?o .
+            }
+            """
+            result_dataset = get(endpoint, query_graph, True)
+
+            num_triples_blazegraph = len(result_dataset)
+            num_triples_local = len(my_graph)
+
+            pos = num_triples_blazegraph == num_triples_local
+            if pos:
+                print("I dati sono stati caricati con successo su Blazegraph.")
+                return True
+            else:
+                print("Caricamento dei dati su Blazegraph non riuscito.")
+                return False
+
 
 '''prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 prefix schema: <http://schema.org/>
@@ -204,7 +223,6 @@ class MetadataQueryHandler(Handler):
         super().__init__()
         self.dbPathOrUrl = db_url
 
-    
     
 
 
@@ -235,10 +253,6 @@ class MetadataQueryHandler(Handler):
 
 
     def getAllPeople(self):
-        grp_dbUrl = "http://192.168.1.153:9999/blazegraph/"   
-        metadata = MetadataUploadHandler()
-        metadata.setDbPathOrUrl(grp_dbUrl)
-        metadata.pushDataToDb("~/Downloads/Data Science/meta.csv")
         query = """
         PREFIX FOAF: <http://xmlns.com/foaf/0.1/>
         PREFIX schema: <http://schema.org/>
@@ -265,10 +279,6 @@ class MetadataQueryHandler(Handler):
     
     
     def getAllCulturalHeritageObjects(self) -> pd.DataFrame:
-        grp_dbUrl = "http://192.168.1.153:9999/blazegraph/"
-        metadata = MetadataUploadHandler()
-        metadata.setDbPathOrUrl(grp_dbUrl)
-        metadata.pushDataToDb("~/Downloads/Data Science/meta.csv")
     
         query = """
         PREFIX schema: <http://schema.org/>
@@ -334,9 +344,6 @@ class MetadataQueryHandler(Handler):
     
     def getAuthorsOfCulturalHeritageObject(self, object_id: str) -> pd.DataFrame:
         # Carica i metadati nel database (assicurati di implementare pushDataToDb correttamente)
-        metadata = MetadataUploadHandler()
-        metadata.setDbPathOrUrl("http://192.168.1.153:9999/blazegraph/")
-        metadata.pushDataToDb("~/Downloads/Data Science/meta.csv")
         
         # Query principale per ottenere gli autori
         query = f"""
@@ -409,10 +416,6 @@ class MetadataQueryHandler(Handler):
 
     
     def getCulturalHeritageObjectsAuthoredBy(personid: str) -> pd.DataFrame:
-        grp_dbUrl = "http://192.168.1.153:9999/blazegraph/"   
-        metadata = MetadataUploadHandler()
-        metadata.setDbPathOrUrl(grp_dbUrl)
-        metadata.pushDataToDb("~/Downloads/Data Science/meta.csv")
         sparql = SPARQLWrapper(grp_dbUrl + "sparql")
 
         query = """
@@ -454,9 +457,8 @@ class MetadataQueryHandler(Handler):
         df = pd.DataFrame(rows, columns=columns)
     
         return df
-  
-    
 
-    
-
-
+grp_dbUrl = "http://192.168.1.8:9999/blazegraph/"
+metadata = MetadataUploadHandler()
+metadata.setDbPathOrUrl(grp_dbUrl)
+metadata.pushDataToDb("data/meta.csv")
